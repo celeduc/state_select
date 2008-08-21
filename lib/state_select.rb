b1514798c292bdeb32e140b6a9981d26912f0192
tony <tony@stark-1.(none)>
2008-08-21 21:38:54 +0100

    ireland regions

--- lib/state_select.rb
+++ lib/state_select.rb
@@ -35,8 +35,9 @@ module ActionView::Helpers::FormOptionsHelper
 "Scottish Borders", "Shetland", "South Ayrshire", "South Lanarkshire", "Stirling", 
 "West Dunbartonshire", "West Lothian", "Western Isles"]  unless const_defined?("SCOTLAND_STATES")
 	WALES_STATES=["Aberystwyth", "Builth Wells", "Brecon Beacons", "Cardiff", "Caernarfon", 
-"Fishguard", "Holyhead", "Llangollen", "Pembrokshire", "Snowdonia", 
-"Swansea"]  unless const_defined?("WALES_STATES")
+"Fishguard", "Holyhead", "Llangollen", "Pembrokshire", "Snowdonia", "Swansea"]  unless const_defined?("WALES_STATES")
+	IRELAND_STATES=["Dublin", "Midlands", "Northern Ireland", "Outer Dublin", "Southeast Ireland", 
+"Southwest Ireland", "West Ireland"]  unless const_defined?("IRELAND_STATES")
 
 end
 
